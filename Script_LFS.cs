3c144a40ae9bb13bebbbad079e9e06e215182f93
XueMei-L <alu0101225845@ull.edu.es>
2024-11-10 14:14:51 +0800

    add more files

---
+++ Script_LFS.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+/// <summary>
+/// This class will be tracked by LFS. It only goal is to print a log
+/// </summary>
+public class Script_LFS : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Script tarea 1.2");
+        
+    }
+}
